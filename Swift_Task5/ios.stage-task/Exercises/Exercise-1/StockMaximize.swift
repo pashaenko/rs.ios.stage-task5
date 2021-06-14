f83ce451db9444ef4e7fd0b79802f8ba330a7896
pashaenko <pashaenko@icloud.com>
2021-06-14 23:21:16 +0300

    goToGit

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -3,6 +3,20 @@ import Foundation
 class StockMaximize {
 
     func countProfit(prices: [Int]) -> Int {
-        return -1
-    }
+        var result = 0
+                
+                for (i, currentValue) in prices.enumerated() {
+                    var maxValue = 0
+                    
+                    for j in i...prices.count - 1 {
+                        if prices[j] > maxValue {
+                            maxValue = prices[j]
+                        }
+                    }
+                    
+                    result += maxValue - currentValue
+                }
+            
+                return result
+            }
 }
